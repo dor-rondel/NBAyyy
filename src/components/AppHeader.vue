50e9efa9a1560250c14584294f448086be52c127
Dor-Ron <dor.rondel@gmail.com>
2018-06-06 21:32:28 -0400

    added project skeleton

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+  v-btn Just to check that vuetify works
+</template>
+
+<script>
+export default {
+  name: 'app-header'
+}
+</script>
+
+<style scoped lang="stylus">
+
+</style>
