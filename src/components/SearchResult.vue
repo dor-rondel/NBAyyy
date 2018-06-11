bd6ddb2142329c40cfa41882d357f414d6eba614
Dor-Ron <dor.rondel@gmail.com>
2018-06-11 00:44:45 -0400

    added store layout

---
+++ src/components/SearchResult.vue
@@ -0,0 +1,36 @@
+<template lang="pug">
+
+</template>
+
+<script>
+export default {
+  name: 'app-search-result',
+
+  data () {
+    return {
+      playerProfile: {
+
+      }
+    }
+  },
+
+  methods: {
+    doFavorite () {
+      console.log('player favorited')
+      // add/remove favorited to firebase
+      // update vuex store
+    }
+  },
+}
+</script>
+
+<style scoped lang="stylus">
+@import url('https://fonts.googleapis.com/css?family=Ubuntu');
+
+.headline, .instructions
+  font-family 'Ubuntu', sans-serif
+
+.headline
+  font-size 40px
+  font-weight bold
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -5,15 +5,91 @@ Vue.use(Vuex)
 
 export const store = new Vuex.Store({
   state: {
-
+    favoritedPlayers: [], // String Array of player names
+    loggedIn: false
   },
+
   getters: {
+    /**
+     * Returns favorited players array from state.
+     *
+     * @returns {Array} of type String
+    */
+    getFavoritedPlayersArray (state) {
+      return state.favoritedPlayers
+    },
 
+    /**
+     * Returns true if user logged in else returns false.
+     *
+     * @returns {Boolean}
+    */
+    userLoggedIn (state) {
+      return state.loggedIn
+    },
+
+    /**
+     * Returns true if player is part of favoritedPlayers
+     * else returns false.
+     *
+     * @param {String} pname the name of the player being searched
+     * @returns {Boolean}
+    */
+    playerIsFavorite (state, pname) {
+      return state.favoritedPlayers.includes(pname)
+    }
   },
-  mutations: {
 
+  mutations: {
+    /** Signs user in. */
+    signUserIn (state) {
+      // Some Firebase stuff
+      state.loggedIn = true
+    },
+
+    /** Signs user out */
+    signUserOut (state) {
+      // Some Firebase stuff
+      state.loggedIn = false
+    },
+
+    /** favorites player */
+    favoritePlayer (state, pname) {
+      // Some Firebase stuff
+      state.favoritedPlayers.push(pname)
+    },
+
+    /** unfavorites player */
+    unfavoritePlayer (state, pname) {
+      // Some Firebase stuff
+      state.favoritedPlayers.splice(state.favoritedPlayers.indexOf(pname), 1)
+    },
+
+    /** sets players favorited array to firebase's version */
+    setFavoriteArray (state) {
+      // Reach out to Firebase
+      // state.favoritedPlayers =
+    }
   },
   actions: {
+    signUserInAct ({commit}) {
+      commit('signUserIn')
+    },
 
+    signUserOutAct ({commit}) {
+      commit('signUserOut')
+    },
+
+    favoritePlayerAct ({commit}, pname) {
+      commit('favoritePlayer', pname)
+    },
+
+    unfavoritePlayerAct ({commit}, pname) {
+      commit('unfavoritePlayer', pname)
+    },
+
+    setFavoriteArrayAct ({commit}) {
+      commit('setFavoriteArray')
+    }
   }
 })
